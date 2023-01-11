3ce88e7882a5184cb12b067a64676929722d6ed1
girma <girmatarekegn.gi@gmail.com>
2023-01-11 11:10:38 +0300

    Add reservation book

--- src/components/rocketList.js
+++ src/components/rocketList.js
@@ -1,8 +1,20 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { bookRocket, removeRocket } from '../Redux/rocketSlice';
 import '../styles/rocketList.css';
 
 const RocketList = (props) => {
-  const { name, description, images } = props;
+  const {
+    name, description, images, id, reserved,
+  } = props;
+  const dispatch = useDispatch();
+
+  const handleAddClick = (id) => {
+    dispatch(bookRocket(id));
+  };
+  const handleCancelClick = (id) => {
+    dispatch(removeRocket(id));
+  };
   return (
     <div className="rocket-list">
       <div className="new-container">
@@ -12,8 +24,30 @@ const RocketList = (props) => {
           </div>
           <div className="media-co">
             <h2 className="title">{name}</h2>
-            <p className="text-dec">{description}</p>
-            <button type="button" className="btn is-info">Reserve Rocket</button>
+
+            <p className="text-dec">
+              {reserved && (
+              <span className="btn btn-primary btn-sm m-1">Reserved</span>
+              )}
+              {description}
+            </p>
+            {reserved ? (
+              <button
+                type="button"
+                onClick={() => handleCancelClick(id)}
+                className="btn btn btn-dark btn-sm m-1"
+              >
+                Cancel Reservation
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => handleAddClick(id)}
+                className="btn btn-primary btn-sm m-1"
+              >
+                Reserve Rocket
+              </button>
+            )}
           </div>
         </div>
       </div>
@@ -22,8 +56,15 @@ const RocketList = (props) => {
 };
 
 RocketList.propTypes = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  images: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  reserved: PropTypes.bool,
 };
+
+RocketList.defaultProps = {
+  reserved: false,
+};
+
 export default RocketList;
