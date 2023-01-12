442eb0632ab09c147ee9e7eba75550bc1feffe9b
unknown <Riyanagwl@gmail.com>
2023-01-12 17:06:05 +0300

    test missions

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import App from './App';
 import './index.css';
 import store from './Redux/configureStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -16,4 +15,5 @@ root.render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
+document.getElementById('root')
 );
